a552eae08bd38fbdc785092616ca624226e525b8
Sundar K <sundar_babu@hotmail.com>
2021-05-14 19:41:24 -0500

    ingress cidr updated

--- main-updated.tf
+++ main-updated.tf
@@ -40,7 +40,7 @@ resource "aws_security_group" "default" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["99.33.74.29"]
+    cidr_blocks = ["18.43.74.89"]
   }
 
 }
