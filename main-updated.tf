967d03437f261809eae034ec93b1b64648be949a
Sundar K <sundar_babu@hotmail.com>
2021-06-15 18:36:35 -0500

    test

--- main-updated.tf
+++ main-updated.tf
@@ -18,7 +18,6 @@ resource "aws_instance" "default" {
     http_tokens                 = "required"
   }
 
-
   tags = {
     Name = "terraform-default"
   }
@@ -30,9 +29,9 @@ resource "aws_security_group" "default" {
   description = "allow http & ssh"
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "https"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "http"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -45,24 +44,115 @@ resource "aws_security_group" "default" {
 
 }
 
+# S3 Bucket to store terraform state
+resource "aws_s3_bucket" "state-bucket" {
+  bucket = "terraform-state-sk"
+  acl    = "private"
+  block_public_acls = true
+  block_public_policy = true
+  restrict_public_buckets = true
+
+  tags = {
+    Name        = "State bucket"
+    Environment = "Dev"
+  }
+}
+
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-state"
+    bucket = "terraform-state-sk"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
 
 # Block Public access for S3 Bucket
-resource "aws_s3_bucket_public_access_block" "terraform-state" {
-    bucket = aws_s3_bucket.terraform-state.id
-    block_public_acls = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
+#resource "aws_s3_bucket_public_access_block" "terraform-state" {
+#    bucket = aws_s3_bucket.terraform-state.id
+#    block_public_acls = true
+#    block_public_policy = true
+#    ignore_public_acls = true
+#    restrict_public_buckets = true
+#}
+
+#}
+
+
+# S3 Bucket to host Static Website
+resource "aws_s3_bucket" "website" {
+  bucket = "s3-website-test.hashicorp.com"
+  acl    = "public-read"
+  policy = file("policy.json")
+  log_bucket = "website-log-sk"
+  
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
 }
 
+# S3 Bucket to store logs
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "website-log-sk"
+  acl    = "log-delivery-write"
 }
+
+# KMS Encryption Key
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+# Server Side Encryption for S3 Bucket
+resource "aws_s3_bucket" "state-bucket" {
+  bucket = "terraform-state-sk"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+
+# Create ECR image repository
+resource "aws_ecr_repository" "devsecops" {
+  name = "devsecops"
+}
+
+output "devsecops-repository URL" {
+  value = "${aws_ecr_repository.devsecops.repository_url}"
+}
+
+# docker built -t https://xxxxxxxx.us-east-1.amazonaws.com/jenkins:1 .
+# `aws ecr get-login`
+# docker push https://xxxxxxxx.us-east-1.amazonaws.com/jenkins:1 
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Enable AWS Shield to protect EIP against "DDoS" attack
 data "aws_availability_zones" "available" {}
