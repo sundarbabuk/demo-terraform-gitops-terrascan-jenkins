504b1b6fdfd4ff49a4ec12fbd1e07e73d8c0daac
sundarbabuk <58757535+sundarbabuk@users.noreply.github.com>
2021-07-07 11:36:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-demo-bucket"
+    bucket = "terraform-state-sk"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
