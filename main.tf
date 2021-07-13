d86d708d5a82c7ea2d3d8a2ad30673b16ef4cfe6
Sundar K <sundar_babu@hotmail.com>
2021-07-13 14:18:34 -0500

    i

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "default" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["99.73.228.255/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
