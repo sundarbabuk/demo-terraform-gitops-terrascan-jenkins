a1e38745a3061526d0d3580fe1d5e561c9cd39e0
Sundar K <sundar_babu@hotmail.com>
2021-07-29 16:34:45 -0500

    port changed

--- main.tf
+++ main.tf
@@ -51,8 +51,8 @@ resource "aws_security_group" "default" {
   }
 
   ingress {
-    from_port   = 34
-    to_port     = 34
+    from_port   = 38
+    to_port     = 38
     protocol    = "tcp"
     cidr_blocks = ["99.73.228.255/32"]
   }
